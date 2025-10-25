76ca4be205cbedd5f66ccf96450a5ee10aea6f9a
Bianca <you@example.com>
2025-10-25 19:12:59 -0300

    refactor: use PolicySummarySerializer in endorsement response

--- app/serializers/endorsement_serializer.rb
+++ app/serializers/endorsement_serializer.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class EndorsementSerializer < ActiveModel::Serializer
   attributes :id, :issue_date, :endorsement_type, :new_sum_insured,
              :new_start_date, :new_end_date, :canceled_endorsement_id,
              :created_at, :updated_at
+
+  belongs_to :policy, serializer: PolicySummarySerializer
 end
